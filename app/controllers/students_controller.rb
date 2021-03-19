718321da3f5fc6f8fe1a7edbaa13efbbda97e2ac
megan <megan.schertz@gmail.com>
2021-03-18 22:41:01 -0500

    commit tree 7d3c75f0806f30b88c8f5ac4b4cdc40b12377323

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -21,10 +21,11 @@ class StudentsController < ApplicationController
   end
 
   def index
-    @students = Student.all
+    @students = Student.search(params[:query])
   end
 
   def student_params
     params.require(:student).permit(:name, :birthday, :hometown)
   end
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,12 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(search_name)
+    if search_name
+      self.where('NAME like ?', "%#{search_name}%")
+    else
+      self.all
+    end
+  end
 end
